d61db30d57df01b0d583f753283d72420e12136c
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-20 18:15:34 +0100

    Completed 2/3 Bonus

--- main.js
+++ main.js
@@ -15,6 +15,11 @@
     Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
         --->>>> Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 
+    BONUS
+    Formattare le date in formato italiano (gg/mm/aaaa)
+    Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).     OK
+    Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.         OK
+
 */
 
 
@@ -58,7 +63,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": "https://unsplash.it/300/300?image="
+            "image": null
         },
         "likes": 56,
         "created": "2021-04-03"
@@ -128,41 +133,82 @@ for (let i = 0; i < likeButtons.length; i++){
 
 // Creo la funzione che stampi i post in pagina
 function printPosts(arr, div) {
+
     // Creo un ciclo che vada a stampare nel Contenitore le Immagini e le loro Descrizioni
-arr.forEach((element, i, array) =>{
+
+    for(let i = 0; i < arr.length; i++) {
+
+    let profileUser;
+
+    if(arr[i].author.image == null) {
+
+        profileUser = document.createElement('div');
+        profileUser.classList.add('profile-user');
+        let initials = arr[i].author.name[0] + arr[i].author.name[foundInitials(arr[i].author.name)];
+        profileUser.append(initials);
+
+    } else {
+
+        profileUser = document.createElement('img');
+        profileUser.classList.add('profile-pic');
+        profileUser.src = arr[i].author.image;
+        profileUser.alt = arr[i].author.name;
+        
+    }
+
+
+
     div.innerHTML += `
                     <div class="post">
                         <div class="post__header">
                             <div class="post-meta">                    
                                 <div class="post-meta__icon">
-                                    <img class="profile-pic" src="${posts[i].author.image}" alt="Phil Mangione">                    
+
                                 </div>
                                 <div class="post-meta__data">
-                                    <div class="post-meta__author">${posts[i].author.name}</div>
-                                    <div class="post-meta__time">${posts[i].created}.</div>
+                                    <div class="post-meta__author">${arr[i].author.name}</div>
+                                    <div class="post-meta__time">${arr[i].created}.</div>
                                 </div>                    
                             </div>
                         </div>
-                        <div class="post__text">${posts[i].content}</div>
+                        <div class="post__text">${arr[i].content}</div>
                         <div class="post__image">
-                            <img src="${posts[i].media}" alt="">
+                            <img src="${arr[i].media}" alt="">
                         </div>
                         <div class="post__footer">
                             <div class="likes js-likes">
                                 <div class="likes__cta">
-                                    <a class="like-button  js-like-button" href="#nogo" data-postid="${posts[i].id}">
+                                    <a class="like-button  js-like-button" href="#nogo" data-postid="${arr[i].id}">
                                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                                         <span class="like-button__label">Mi Piace</span>
                                     </a>
                                 </div>
                                 <div class="likes__counter">
-                                    Piace a <b id="like-counter-${posts[i].id}" class="js-likes-counter">${posts[i].likes}</b> persone
+                                    Piace a <b id="like-counter-${arr[i].id}" class="js-likes-counter">${arr[i].likes}</b> persone
                                 </div>
                             </div> 
                         </div>            
                     </div>
                     `
-})
+
+    const postMeta = document.querySelector('.post:last-child .post-meta__icon');
+    postMeta.append(profileUser);
+
+    }
 
 }
 
+
+// Creo una funzione che cicli delle stringe in cui do per scontato ci sia uno "spazio" (es. tra Nomi e Cognomi)
+function foundInitials(str) {
+
+    let position = 0;
+
+        for (let j = 0; j < str.length; j++) {
+
+        if (str[j] == " ") {
+            position = j + 1;
+            return(position);
+        }
+    }
+}
