a98c2fffe219a9b22a3a8fc59bc3609f49e50ad2
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-20 14:22:09 +0100

    Create event to the Like Button

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+/*  
+
+    Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post.                                                        OK
+        Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+            - id del post, numero progressivo da 1 a n
+            - nome autore,
+            - foto autore,
+            - data in formato americano (mm-gg-yyyy),
+            - testo del post,
+            - immagine (non tutti i post devono avere una immagine),
+            - numero di likes.
+
+    Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.                     OK
+
+    Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+        --->>>> Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+*/
+
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +75,73 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+// Dichiaro una Variabile che selezioni dall'HTML il contenitore dei Post
+const container = document.getElementById('container');
+console.log('Questo è il contenitore dei post', container, typeof container);
+
+// Stampo i post in pagina
+printPosts(posts, container);
+
+// Seleziono i tasto dei "Mi Piace" dall'HTML
+const likeButton = document.querySelectorAll('.js-like-button');
+
+// Creo un ciclo che scorra l'Array dei pulsanti con classe "js-like-button"
+for (let i = 0; i < likeButton.length; i++){
+
+        // Creo l'evento 'click' sul bottone i-esimo
+        likeButton[i].addEventListener('click', function(){
+
+        // Aggiungo la classe per cui il bottone i-esimo si colori di verde una volta clickato
+        likeButton[i].classList.add('like-button--liked');
+        console.log('Cliccato Mi Piace')
+    });
+
+};
+
+/*
+
+    FUNZIONI
+
+*/
+
+// Creo la funzione che stampi i post in pagina
+function printPosts(arr, div) {
+    // Creo un ciclo che vada a stampare nel Contenitore le Immagini e le loro Descrizioni
+arr.forEach((element, i, array) =>{
+    div.innerHTML += `
+                        <div class="post">
+                            <div class="post__header">
+                                <div class="post-meta">                    
+                                    <div class="post-meta__icon">
+                                        <img class="profile-pic" src="${posts[i].author.image}" alt="Phil Mangione">                    
+                                    </div>
+                                    <div class="post-meta__data">
+                                        <div class="post-meta__author">${posts[i].author.name}</div>
+                                        <div class="post-meta__time">${posts[i].created}.</div>
+                                    </div>                    
+                                </div>
+                            </div>
+                            <div class="post__text">${posts[i].content}</div>
+                            <div class="post__image">
+                                <img src="${posts[i].media}" alt="">
+                            </div>
+                            <div class="post__footer">
+                                <div class="likes js-likes">
+                                    <div class="likes__cta">
+                                        <a class="like-button  js-like-button" href="#" data-postid="${posts[i].id}">
+                                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                            <span class="like-button__label">Mi Piace</span>
+                                        </a>
+                                    </div>
+                                    <div class="likes__counter">
+                                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                                    </div>
+                                </div> 
+                            </div>            
+                        </div>
+                    `
+})
+
+}
+
